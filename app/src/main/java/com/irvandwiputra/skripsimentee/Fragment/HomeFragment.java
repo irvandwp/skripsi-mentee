4996b8c7c33631c2f7181ac2f7cb9ec395871d37
Irvan Dwi Putra <hevanorvin@gmail.com>
2017-06-01 21:18:43 +0700

    Create order activity

--- app/src/main/java/com/irvandwiputra/skripsimentee/Fragment/HomeFragment.java
+++ app/src/main/java/com/irvandwiputra/skripsimentee/Fragment/HomeFragment.java
@@ -1,30 +1,50 @@
 package com.irvandwiputra.skripsimentee.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.irvandwiputra.skripsimentee.OrderActivity;
 import com.irvandwiputra.skripsimentee.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
+    @Bind(R.id.buttonOrder)
+    Button buttonOrder;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+
+        buttonOrder.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonOrder:
+                Intent intent = new Intent(getContext(), OrderActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
