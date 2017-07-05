b7fed5bba5d665fa0579bed11b58ca82857469ee
Alvin Oktavianus <agentsam1995@gmail.com>
2017-07-06 00:02:16 +0700

    Create on long click item in recyclerview

--- app/src/main/java/com/irvandwiputra/skripsimentee/Fragment/HistoryFragment.java
+++ app/src/main/java/com/irvandwiputra/skripsimentee/Fragment/HistoryFragment.java
@@ -2,16 +2,20 @@ package com.irvandwiputra.skripsimentee.Fragment;
 
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.irvandwiputra.skripsimentee.Model.Order;
 import com.irvandwiputra.skripsimentee.Model.ResponseStatus;
@@ -35,6 +39,75 @@ import okhttp3.Response;
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends Fragment {
+
+    interface ClickListener {
+        void onCLick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private HistoryFragment.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final HistoryFragment.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+                @Override
+                public boolean onDown(MotionEvent e) {
+                    return false;
+                }
+
+                @Override
+                public void onShowPress(MotionEvent e) {
+
+                }
+
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return false;
+                }
+
+                @Override
+                public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                    return false;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onCLick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                    return false;
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View view = rv.findChildViewUnder(e.getX(), e.getY());
+            if (view != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onCLick(view, rv.getChildPosition(view));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
 
     private ProgressDialog progressDialog;
     public static final String TAG = HistoryFragment.class.getSimpleName();
@@ -110,6 +183,18 @@ public class HistoryFragment extends Fragment {
                                 historyRecyclerView.setAdapter(historyAdapter);
                                 historyRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
                                 historyRecyclerView.addItemDecoration(itemDecoration);
+                                historyRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(), historyRecyclerView, new ClickListener() {
+                                    @Override
+                                    public void onCLick(View view, int position) {
+                                        Order order = orderArrayList.get(position);
+                                        Toast.makeText(getContext(), "Selected item id is " + order.getId(), Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void onLongClick(View view, int position) {
+
+                                    }
+                                }));
                             }
                         });
                     } else {
