f8672942b6157f319715fe6fdd0472e9d3b8dde1
Alvin Oktavianus <agentsam1995@gmail.com>
2017-07-27 00:10:15 +0700

    Add button listener to the correct feedback class

--- app/src/main/java/com/irvandwiputra/skripsimentee/Feedback.java
+++ app/src/main/java/com/irvandwiputra/skripsimentee/Feedback.java
@@ -1,13 +1,43 @@
 package com.irvandwiputra.skripsimentee;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Feedback extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class Feedback extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.btnSubmit)
+    public Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        ButterKnife.bind(this);
+        buttonSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSubmit:
+                switch (v.getId()) {
+                    case R.id.btnSubmit:
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                                .setMessage("Successfully submit your rating. Thank you")
+                                .setPositiveButton("OK", null);
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                        break;
+                }
+
+                break;
+        }
     }
 }
